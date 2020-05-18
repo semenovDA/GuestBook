295ff1ddfcfabe050704af57d4e41c9e488da481
BlackJack228 <bigblackjack228@gmail.com>
2020-05-18 15:33:14 +0800

    Fixing uuid import package loading error on HeroKu

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const uuid = require('uuid/v1');
+const uuid = require('uuid/dist/v1');
 const Reviews = require('../core/functions/reviews');
 const Review = require('../core/models/review.model');
 const router = express.Router();
